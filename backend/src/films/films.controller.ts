0aeb400b25deacfb1931a2faeff7094bb8dd792b
Andrey Vyazovov <andreyy-vjazovov@rambler.ru>
2025-02-12 15:11:52 +0300

    feat: fix films.controller add entities

--- backend/src/films/films.controller.ts
+++ backend/src/films/films.controller.ts
@@ -5,18 +5,20 @@ import {
   FilmScheduleParams,
   GetScheduleDto,
 } from './dto/films.dto';
+import { Film } from './dto/entities/film.entity';
+import { Schedules } from './dto/entities/schedule.entity';
 
 @Controller('films')
 export class FilmsController {
   constructor(private readonly filmsService: FilmsService) {}
   @Get('/')
-  getFilms(): Promise<{ total: number; items: GetFilmDto[] }> {
+  getFilms(): Promise<{ total: number; items: GetFilmDto[] | Film[] }> {
     return this.filmsService.findAll();
   }
   @Get(':id/schedule')
   getFilmSchedule(
     @Param() params: FilmScheduleParams,
-  ): Promise<{ total: number; items: GetScheduleDto[] }> {
+  ): Promise<{ total: number; items: GetScheduleDto[] | Schedules[] }> {
     return this.filmsService.findById(params.id);
   }
 }
--- backend/src/order/order.module.ts
+++ backend/src/order/order.module.ts
@@ -1,14 +1,18 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { ConfigModule } from '@nestjs/config';
 import { OrdersRepository } from '../repository/order.repository';
 import { OrderService } from './order.service';
 import { FilmsModule } from '../films/films.module';
 
-@Module({
-  imports: [ConfigModule, FilmsModule],
-  controllers: [OrderController],
-  providers: [OrdersRepository, OrderService],
-  exports: [OrderService, OrdersRepository],
-})
-export class OrderModule {}
+@Module({})
+export class OrderModule {
+  static forRootAsync(): DynamicModule {
+    return {
+      module: OrderModule,
+      imports: [ConfigModule, FilmsModule],
+      controllers: [OrderController],
+      providers: [OrdersRepository, OrderService],
+    };
+  }
+}
